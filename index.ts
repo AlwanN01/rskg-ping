4fc0b318331c0fbdd4ebef9bfebd3c9437b8d85c
AlwanN01 <alwannabawi112233@gmail.com>
2023-02-10 16:19:19 +0700

    update host seed

--- index.ts
+++ index.ts
@@ -10,12 +10,12 @@ const server = http.createServer(app)
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`)
-  // ;(async () => {
-  //   await seed.down({ to: 0 }) // yarn seed down --to 0
-  //   await migrate.down({ to: 0 }) // yarn migrate down --to 0
-  //   await db.sequelize.sync({ force: true })
-  //   await migrate.up() // yarn migrate up
-  //   await seed.up() // yarn seed up
-  // })()
+  ;(async () => {
+    await seed.down({ to: 0 }) // yarn seed down --to 0
+    await migrate.down({ to: 0 }) // yarn migrate down --to 0
+    await db.sequelize.sync({ force: true })
+    await migrate.up() // yarn migrate up
+    await seed.up() // yarn seed up
+  })()
 })
 Ping(server)
--- src/db/seeders/2023.01.26T19.00.22.seedHost.ts
+++ src/db/seeders/2023.01.26T19.00.22.seedHost.ts
@@ -1,5 +1,12 @@
 export const up: MigrationFn = async ({ db: { Host } }) => {
-  const host = await Host.create({ hostName: 'example1.com', user: 'alwan', divisi: 'IT' })
+  const host = await Host.bulkCreate([
+    { hostName: 'example1.com', user: 'sample1', divisi: 'Informasi' },
+    { hostName: 'example2.com', user: 'sample2', divisi: 'Informasi' },
+    { hostName: 'example3.com', user: 'sample3', divisi: 'Informasi' },
+    { hostName: '192.168.4.29', user: 'Alwan', divisi: 'IT' },
+    { hostName: '192.168.4.6', user: 'Ari', divisi: 'IT' },
+    { hostName: '192.168.4.7', user: 'Ayu', divisi: 'IT' }
+  ])
   // await host.createPingLog({ isConnect: false })
 }
 export const down: MigrationFn = async ({ db }) => {
